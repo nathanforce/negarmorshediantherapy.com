685db560572f20630e0bdccc6c529080ebe81765
Nathan Force <nathan.a.force@gmail.com>
2018-02-24 13:48:40 -0500

    netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    siteMetadata: {
-        title: "Gatsby Default Starter"
+  siteMetadata: {
+    title: "Gatsby Default Starter"
+  },
+  plugins: [
+    "gatsby-plugin-glamor",
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
     },
-    plugins: [
-        "gatsby-plugin-glamor",
-        "gatsby-plugin-netlify-cms",
-        "gatsby-plugin-react-helmet",
-        "gatsby-transformer-sharp",
-        "gatsby-plugin-sharp",
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "src",
-                path: `${__dirname}/src/`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography.js`
-            }
-        }
-    ]
+    {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography.js"
+      }
+    }
+  ]
 };
