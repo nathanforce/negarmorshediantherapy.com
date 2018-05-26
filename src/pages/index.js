c6a5b8e472576ebe57d770b7a24f7b76018f134d
Nathan Force <nathan.a.force@gmail.com>
2018-05-26 07:46:29 -0400

    homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Img from "gatsby-image";
 import { colors } from "../utils/theme";
-
+import { after } from "glamor";
 export default ({ data }) => (
   <div>
     <div style={{ position: "relative", height: "100vh" }}>
@@ -29,47 +29,74 @@ export default ({ data }) => (
           flexDirection: "column"
         }}
       >
-        <h1
-          css={{
-            margin: 0,
-            fontSize: 200,
-            textTransform: "uppercase",
-            textAlign: "center",
-            lineHeight: 1.5,
-            zIndex: 100,
-            mixBlendMode: "overlay",
-            background: colors.secondary,
-            padding: "2rem",
-            letterSpacing: "initial"
-          }}
-        >
-          NM
-        </h1>
-        <h2
+        <div>
+          <h1
+            css={{
+              margin: 0,
+              fontSize: 200,
+              textTransform: "uppercase",
+              letterSpacing: "initial",
+              mixBlendMode: "overlay",
+              lineHeight: 1,
+              position: "absolute",
+              transform: "translate(0,-30%)",
+              opacity: 0.6
+            }}
+          >
+            NM
+          </h1>
+          <h2
+            style={{
+              color: colors.white,
+              mixBlendMode: "overlay",
+              fontSize: 50,
+              textTransform: "uppercase",
+              fontWeight: 100,
+              marginTop: "2rem",
+              marginBottom: "1rem"
+            }}
+          >
+            Negar Morshedian
+          </h2>{" "}
+          <h2
+            style={{
+              color: colors.white,
+              mixBlendMode: "overlay",
+              fontSize: 50,
+              textTransform: "uppercase",
+              fontWeight: 300,
+              margin: 0
+            }}
+          >
+            Psychotherapist, NYC
+          </h2>
+        </div>
+        <div
           style={{
-            color: colors.white,
-            mixBlendMode: "overlay",
-            fontSize: 50,
-            textTransform: "uppercase",
-            fontWeight: 100,
-            marginTop: "2rem",
-            marginBottom: "1rem"
+            maxWidth: "40%",
+            marginTop: "1rem"
           }}
         >
-          Negar Morshedian
-        </h2>{" "}
-        <h2
-          style={{
-            color: colors.white,
-            mixBlendMode: "overlay",
-            fontSize: 50,
-            textTransform: "uppercase",
-            fontWeight: 300,
-            margin: 0
-          }}
-        >
-          Psychotherapist, NYC
-        </h2>
+          <p
+            style={{
+              color: colors.white,
+              // opacity: 0.8,
+              mixBlendMode: "overlay",
+              background: colors.secondary,
+              display: "inline",
+              padding: "0.45rem",
+              boxDecorationBreak: "clone",
+              WebkitBoxDecorationBreak: "clone"
+            }}
+          >
+            Negar is a compassionate therapist who aims to create an accepting
+            and validating space for her clients. She believes that all humans
+            are wired for healing and growth and her approach to therapy is
+            strength-based, emotion-focused, and relational. Negar works
+            collaboratively with her clients to set goals, identify barriers to
+            growth, and increase coping skills.
+          </p>
+        </div>
       </div>
     </div>
   </div>
