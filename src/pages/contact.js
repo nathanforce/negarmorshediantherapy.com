bdcdeedf08ba661d43ffad136856c36cd05ba441
Nathan Force <nathan.a.force@gmail.com>
2018-05-27 10:09:49 -0400

    contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,74 @@
 import React from "react";
+import { Field, Formik } from "formik";
+import { colors } from "../utils/theme";
+
+import "formbase";
 
 export default () => {
   return (
     <div>
       <h1>Contact</h1>
-      <div>@ me</div>
+      <Formik initialValues={{ name: "", email: "", message: "" }}>
+        {({ values, handleChange, handleBlur }) => (
+          <form
+            name="contact"
+            netlify-honeypot="bot-field"
+            method="POST"
+            action="thank-you"
+            netlify
+          >
+            <p css={{ display: "none" }}>
+              <label>
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+            </p>
+            <label htmlFor="name" className="label">
+              Name
+            </label>
+            <input
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className="input"
+              id="name"
+              name="name"
+              type="text"
+            />
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+            <input
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className="input"
+              id="email"
+              name="email"
+              type="text"
+            />
+            <label htmlFor="message" className="label">
+              Message
+            </label>
+            <textarea
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className="input"
+              id="message"
+              name="message"
+              type="text"
+            />
+            <button
+              css={{
+                backgroundColor: colors.secondary,
+                color: colors.white,
+                padding: ".5 rem 1rem",
+                minWidth: 180
+              }}
+              type="submit"
+            >
+              Send
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
